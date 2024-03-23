b6a84e7ac0bc97d579bb1286ccccf25da22d8fe8
lemon <lemon.juice9341@gmail.com>
2024-03-23 21:31:49 +0300

    clean up project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("kotlin-parcelize")
-    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
     kotlin("kapt")
 }
 
@@ -43,24 +42,21 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
     implementation("com.google.android.gms:play-services-maps:18.2.0")
-    implementation("com.google.android.libraries.places:places:3.4.0")
+
+    implementation("com.squareup.okhttp3:okhttp:4.10.0")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation("com.google.code.gson:gson:2.10.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
-
-    implementation("androidx.coordinatorlayout:coordinatorlayout:1.2.0")
 }
--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -1,25 +1,16 @@
 package com.example.weather
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.weather.databinding.ActivityMainBinding
-import com.google.android.gms.common.api.Status
-import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.libraries.places.api.Places
-import com.google.android.libraries.places.api.model.Place
-import com.google.android.libraries.places.widget.AutocompleteSupportFragment
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private var map: GoogleMap? = null
     private lateinit var binding: ActivityMainBinding
-    private lateinit var autocompleteFragment: AutocompleteSupportFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,35 +20,6 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment =
             supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
         mapFragment.getMapAsync(this)
-
-        Places.initialize(applicationContext, getString(R.string.map_api_kai))
-        autocompleteFragment =
-            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
-        autocompleteFragment.setPlaceFields(
-            listOf(
-                Place.Field.ID,
-                Place.Field.ADDRESS,
-                Place.Field.LAT_LNG,
-                Place.Field.NAME
-            )
-        )
-        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
-            override fun onError(status: Status) {
-                Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onPlaceSelected(place: Place) {
-                val id = place.id
-                val add = place.address
-                val latLng = place.latLng
-                latLng?.let { zoomOnMap(it) }
-            }
-        })
-    }
-
-    fun zoomOnMap(latLng: LatLng) {
-        val newLatLngZoom = CameraUpdateFactory.newLatLngZoom(latLng, 12f)
-        map?.animateCamera(newLatLngZoom)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
