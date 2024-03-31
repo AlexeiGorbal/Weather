f21e46626293d58885cdd500ad3e6f4b2770ecb9
lemon <lemon.juice9341@gmail.com>
2024-03-31 22:14:10 +0300

    remove parcelise from LocationInfo

--- app/src/main/java/com/example/weather/location/LocationInfo.kt
+++ app/src/main/java/com/example/weather/location/LocationInfo.kt
@@ -1,13 +1,9 @@
 package com.example.weather.location
 
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
 data class LocationInfo(
     val id: Long,
     val name: String,
     val country: String,
     val lat: Double,
     val lon: Double
-) : Parcelable
+)
