293e9d12d694e4ae82f03519bf80c6ddc763fcd0
lemon <lemon.juice9341@gmail.com>
2024-04-02 18:36:31 +0300

    add day field to to DayWeather

--- app/src/main/java/com/example/weather/weather/DayWeather.kt
+++ app/src/main/java/com/example/weather/weather/DayWeather.kt
@@ -2,9 +2,9 @@ package com.example.weather.weather
 
 data class DayWeather(
     val weatherIcon: String,
+    val day: String,
     val weatherState: String,
     val minTempF: Float,
     val maxTempF: Float,
     val hourlyForecast: List<HourlyWeather>
 )
-//TODO val day
